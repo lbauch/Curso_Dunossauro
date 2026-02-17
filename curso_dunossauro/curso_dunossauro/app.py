2563ff52e6e6d9a1c9a3ad490983cb466399ff59
lbauch <bauchspiess.lucas@gmail.com>
2026-02-17 11:57:41 -0300

    feat: Curso Dunossauro aula2 - Fundamentos de web.

--- curso_dunossauro/curso_dunossauro/app.py
+++ curso_dunossauro/curso_dunossauro/app.py
@@ -1,11 +1,30 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from curso_dunossauro.schemas import Message
+
+app = FastAPI(title="Api curso Dunossauro", version="0.1.0")
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     """
     Função olá mundo. Testando documentação
     """
-    return {"message": "Olá Mundo!"}
+    # return {"message": "Olá Mundo!"} - Também é possível enviar desta forma
+    return Message(message="Olá Mundo!")
+
+
+@app.get("/html", response_class=HTMLResponse)
+def read_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- curso_dunossauro/tests/test_app.py
+++ curso_dunossauro/tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {"message": "Olá Mundo!"}  # Asset
+
+
+def test_html_page():
+    client = TestClient(app)  # Arrange
+
+    response = client.get("/html")  # Act
+
+    assert response.status_code == HTTPStatus.OK
+    assert "Olá Mundo" in response.text
